5c519a886c87bbf07dfbf9f28f9719a88b2e0cd7
Osmário Júnior <osmariojj@gmail.com>
2022-07-18 23:08:12 -0300

    add Models

---
+++ src/models/models.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from typing import Optional, List
+
+class User(BaseModel):
+    id: Optional[str] = None
+    name: str
+    tel: str
+    my_products: List[Product]
+    my_sales: List[Product]
+    my_requests: List[RequestsOrder]
+
+class Product(BaseModel):
+    id: Optional[str] = None
+    user: User
+    name: str
+    details: str
+    price: float
+    available: bool = False
+
+class RequestsOrder(BaseModel):
+    id: Optional[str] = None
+    user: User
+    product: Product
+    amount: int
+    delivery: bool = True
+    address: str
+    comments: Optional[str] = 'Sem observações'
